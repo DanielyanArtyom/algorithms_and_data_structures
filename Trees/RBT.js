97ae599dc8a41387621ed14201a3a69ba1ecb7cd
ArtyomDanielyan <artyom.danielyan1@gmail.com>
2023-06-10 23:45:53 +0400

    added deletion for RBT

--- Trees/RBT.js
+++ Trees/RBT.js
@@ -1,3 +1,49 @@
+class Queue {
+  _queue = null;
+
+  constructor() {
+    this._queue = [];
+  }
+
+  push(element) {
+    if (!this._queue.length) {
+      this._queue.push(element);
+    } else {
+      this._queue = [...this._queue, element];
+    }
+  }
+
+  pop() {
+    if (!this._queue.length) {
+      return null;
+    }
+    let tmpArray = new Array(this._queue.length - 1).fill(0);
+    for (let i = 1; i < this._queue.length; ++i) {
+      tmpArray[i - 1] = this._queue[i];
+    }
+
+    let poppedElement = this._queue[0];
+
+    this._queue = tmpArray;
+    return poppedElement;
+  }
+
+  front() {
+    return this._queue[0];
+  }
+  back() {
+    return this._queue[this._queue.length - 1];
+  }
+
+  size() {
+    return this._queue.length;
+  }
+
+  empty() {
+    this._queue = [];
+  }
+}
+
 const TreeColors = {
   RED: "red",
   BLACK: "black",
@@ -28,6 +74,31 @@ class TreeNode {
 class RBG {
   root = null;
   NIL = new TreeColors(null, TreeColors.BLACK);
+
+  levelOrderTraversal() {
+    if (!root) {
+      return "Tree is empty";
+    }
+    let queue = new Queue();
+    queue.push(root);
+    while (queue.size()) {
+      let size = queue.size();
+      while (size !== 0) {
+        let current = queue.pop();
+        if (current) {
+          console.log(`val: ${current.val}, color: ${current.color}`);
+          if (current.left) {
+            queue.push(current.left);
+          }
+          if (current.right) {
+            queue.push(current.right);
+          }
+        }
+        --size;
+      }
+    }
+    queue.empty();
+  }
 
   rightRotate(treeNode) {
     let y = treeNode.left;
@@ -150,5 +221,145 @@ class RBG {
 
       this.root.color = TreeColors.BLACK;
     }
+  }
+
+  search(target) {
+    return this.#search(this.root, target);
+  }
+
+  #search(treeNode, target) {
+    if (!treeNode) {
+      return null;
+    }
+    if (treeNode.val === target) {
+      return treeNode;
+    }
+    if (treeNode.val > target) {
+      return this.#search(treeNode.left, target);
+    }
+    return this.#search(treeNode.right, target);
+  }
+
+  #transplant(u, v) {
+    if (u.parent === this.NIL) {
+      root = v;
+    } else if (u === u.parent.left) {
+      u.parent.left = v;
+    } else {
+      u.parent.right = v;
+    }
+    v.parent = u.parent;
+  }
+
+  delete(target) {
+    let nodeToDelete = this.#search(target);
+
+    let y = nodeToDelete;
+
+    let y_original_color = y.color;
+    let x = null;
+    if (nodeToDelete.left === this.NIL) {
+      x = nodeToDelete.right;
+      this.#transplant(nodeToDelete, nodeToDelete.right);
+    } else if (nodeToDelete.right === this.NIL) {
+      x = nodeToDelete.left;
+      this.#transplant(nodeToDelete, nodeToDelete.left);
+    } else {
+      y = this.#getMinimum(nodeToDelete.right);
+      y_original_color = y.color;
+      x = y.right;
+
+      if (x.parent === nodeToDelete) {
+        x.parent = y;
+      } else {
+        this.#transplant(y, y.right);
+        y.right = nodeToDelete.right;
+        y.right.parent = y;
+      }
+      this.#transplant(nodeToDelete, y);
+
+      y.left = nodeToDelete.left;
+      y.left.parent = y;
+      y.color = nodeToDelete.color;
+    }
+
+    if (y_original_color === TreeColors.BLACK) {
+      this.#deleteFixup(y.right);
+    }
+  }
+
+  deleteFixup(x) {
+    while (x !== this.root && x.color === TreeColors.BLACK) {
+      if (x === x.parent.left) {
+        let brotherNode = x.parent.right;
+        if (brotherNode.color === TreeColors.RED) {
+          //case 1
+          brotherNode.color = TreeColors.BLACK;
+          x.parent.color = TreeColors.RED;
+          this.leftRotate(x.parent);
+          brotherNode = x.parent.right;
+        }
+        if (
+          // case 2
+          brotherNode.left.color === TreeColors.BLACK &&
+          brotherNode.right.color === TreeColors.BLACK
+        ) {
+          brotherNode.color = TreeColors.RED;
+          x = x.parent;
+        } else {
+          if (brotherNode.right.color === TreeColors.BLACK) {
+            //case 3
+            brotherNode.left.color = TreeColors.BLACK;
+            brotherNode.color = TreeColors.RED;
+            this.rightRotate(brotherNode);
+            brotherNode = x.parent.right;
+          }
+          brotherNode.color = x.parent.color; //case 4
+          x.parent.color = TreeColors.BLACK;
+          brotherNode.right.color = TreeColors.BLACK;
+          this.leftRotate(x.parent);
+          x = this.root;
+        }
+      } else {
+        let brotherNode = x.parent.left;
+        if (brotherNode.color === TreeColors.RED) {
+          //case 1
+          brotherNode.color = TreeColors.BLACK;
+          x.parent.color = TreeColors.RED;
+          this.rightRotate(x.parent);
+          brotherNode = x.parent.left;
+        }
+        if (
+          brotherNode.left.color === TreeColors.BLACK && //case 2
+          brotherNode.right.color === TreeColors.BLACK
+        ) {
+          brotherNode.color = TreeColors.RED;
+          x = x.parent;
+        } else {
+          if (brotherNode.left.color === TreeColors.BLACK) {
+            //case 3
+            brotherNode.right.color = TreeColors.BLACK;
+            brotherNode.color = TreeColors.RED;
+            this.leftRotate(brotherNode);
+            brotherNode = x.parent.left;
+          }
+          brotherNode.color = x.parent.color; //case 4
+          x.parent.color = TreeColors.BLACK;
+          brotherNode.left.color = TreeColors.BLACK;
+          this.leftRotate(x.parent);
+          x = this.root;
+        }
+      }
+
+      x.color = TreeColors.BLACK;
+    }
+  }
+
+  #getMinimum(treeNode) {
+    if (!treeNode.left) {
+      return treeNode;
+    }
+
+    return this.#getMinimum(treeNode.left);
   }
 }
